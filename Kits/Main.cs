9e2c00c80b4acca03f287e4c51ee02480b597c02
BuildTools <unconfigured@null.spigotmc.org>
2023-01-07 18:49:27 -0300

    Ver 1.0.1

--- Kits/Main.cs
+++ Kits/Main.cs
@@ -8,7 +8,7 @@
     {
         public override string Name => "Kits";
         public override string Author => "BraZa#5713";
-        public override Version Version { get; } = new Version(1, 0, 0);
+        public override Version Version { get; } = new Version(1, 0, 1);
         public override Version RequiredExiledVersion { get; } = new Version(6, 0, 0);
 
         public static Main Singleton;
